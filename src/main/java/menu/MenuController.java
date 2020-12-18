3673ad42d0afd2aeef9d74130d2e2509205f592d
KJunseo <max9106@naver.com>
2020-12-18 18:13:15 +0900

    refactor: 해당 테이블에 선택한 메뉴가 99개인지 확인하는 메소드 수정

--- src/main/java/menu/MenuController.java
+++ src/main/java/menu/MenuController.java
@@ -7,6 +7,8 @@ import view.OutputView;
 import java.util.List;
 
 public class MenuController {
+    private static final String ALREADY_MAXIMUM_AMOUNT = "[ERROR] 최대 수량은 99개입니다.";
+
     public static void showMenu() {
         List<Menu> menus = MenuService.allMenus();
         OutputView.printMenus(menus);
@@ -25,9 +27,9 @@ public class MenuController {
 
     public static int menuAmount(Table table, Menu menu) {
         OutputView.amountMenu();
-        if (table.canAdditionalOrder(menu)) {
-            return InputView.amountMenu(table, menu);
+        if (!table.canAdditionalOrder(menu)) {
+            throw new IllegalArgumentException(ALREADY_MAXIMUM_AMOUNT);
         }
-        return 0;
+        return InputView.amountMenu(table, menu);
     }
 }
