d301294fe7f2c20449f55c922ee17154b2f03bc8
xfrnk2 <xfrnk2@gmail.com>
2021-12-18 08:50:58 +0900

    refactor: 미리 작성되어 있던 코드 제거

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,3 @@
-import domain.Menu;
-import domain.MenuRepository;
-import domain.Table;
-import domain.TableRepository;
-import view.InputView;
-import view.OutputView;
-
-import java.util.List;
 
 public class Application {
     // TODO 구현 진행
