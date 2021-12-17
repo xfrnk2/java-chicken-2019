4cbd13583ae0201f5e408f68aee86a3950ffbedb
xfrnk2 <xfrnk2@gmail.com>
2021-12-17 14:38:32 +0900

    refactor(InputView, OutputView): 정적 메서드가 아닌 형태로 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -10,7 +10,7 @@ public class OutputView {
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
 
-    public static void printTables(final List<Table> tables) {
+    public void printTables(final List<Table> tables) {
         System.out.println("## 테이블 목록");
         final int size = tables.size();
         printLine(TOP_LINE, size);
@@ -18,20 +18,20 @@ public class OutputView {
         printLine(BOTTOM_LINE, size);
     }
 
-    public static void printMenus(final List<Menu> menus) {
+    public void printMenus(final List<Menu> menus) {
         for (final Menu menu : menus) {
             System.out.println(menu);
         }
     }
 
-    private static void printLine(final String line, final int count) {
+    private void printLine(final String line, final int count) {
         for (int index = 0; index < count; index++) {
             System.out.print(line);
         }
         System.out.println();
     }
 
-    private static void printTableNumbers(final List<Table> tables) {
+    private void printTableNumbers(final List<Table> tables) {
         for (final Table table : tables) {
             System.out.printf(TABLE_FORMAT, table);
         }
