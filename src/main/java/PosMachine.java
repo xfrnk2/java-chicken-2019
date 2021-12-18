b800cceeb004ed484916f120fdda5281da888a13
xfrnk2 <xfrnk2@gmail.com>
2021-12-18 09:37:57 +0900

    refactor: 메인 화면에서 원하는 기능을 입력받는 기능을 별도로 분리

--- src/main/java/PosMachine.java
+++ src/main/java/PosMachine.java
@@ -1,3 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import common.MainCategory;
+import controller.OrderController;
 import view.InputView;
 import view.OutputView;
 
@@ -10,10 +16,17 @@ public class PosMachine {
 			try {
 				outputView.printMainMenus();
 				outputView.printSelectMainMenuRequest();
-				String option = inputView.selectMainMenu();
+				String option = askOptionChoice();
+
 			} catch (IllegalArgumentException e) {
 				System.out.println(e.getMessage());
 			}
 		}
+	}
+
+	private String askOptionChoice() {
+		List<MainCategory> optionList = Arrays.asList(MainCategory.values());
+		String option = inputView.selectMainMenu(optionList);
+		return option;
 	}
 }
